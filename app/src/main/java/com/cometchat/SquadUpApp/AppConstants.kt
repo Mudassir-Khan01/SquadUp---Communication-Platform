a7cd3c7eaa46cb146e538d8828cabf10efd9ea33
Mudassir Khan <138046650+Mudassir-Khan01@users.noreply.github.com>
2024-11-25 23:38:56 +0530

    SquadUp

--- app/src/main/java/com/cometchat/SquadUpApp/AppConstants.kt
+++ app/src/main/java/com/cometchat/SquadUpApp/AppConstants.kt
@@ -2,10 +2,10 @@ package com.cometchat.SquadUpApp
 
 object AppConstants {
 
-    const val APP_ID = "2664190ec3504ef2"
+    const val APP_ID = "*"
 
-    const val AUTH_KEY = "986218d4afce65a003d467732ab56952dad3b892"
+    const val AUTH_KEY = "********************"
 
-    const val REGION = "in"
+    const val REGION = "**"
 
 }
